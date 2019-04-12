013afd718dc383881fe6167325c2f77e5cd78252
Duong Seyha <duongseyha.bb@gmail.com>
2019-04-12 10:28:10 +0700

    change login style

--- resources/views/cms/login.blade.php
+++ resources/views/cms/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Admin Template</title>
+    <title>Dashboard</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -10,13 +10,13 @@
     <meta name="author" content="colorlib" />
     <!-- Favicon icon -->
     <link rel="icon" href="https://colorlib.com/polygon/admindek/plugin/assets/images/favicon.ico" type="image/x-icon">
-    <!-- Google font-->     
+    <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
     <!-- Required Fremwork -->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/bower_components/bootstrap/css/bootstrap.min.css') }}">
    <!-- waves.css -->
     <link rel="stylesheet" href="{{ asset('plugin/cms/assets/pages/waves/css/waves.min.css') }}" type="text/css" media="all">
-    <!-- feather icon --> 
+    <!-- feather icon -->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/icon/feather/css/feather.css') }}">
     <!-- themify-icons line icon -->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugin/cms/assets/icon/themify-icons/themify-icons.css') }}">
@@ -56,7 +56,7 @@
                       <div class="circle"></div>
                   </div>
               </div>
-            
+
               <div class="spinner-layer spinner-yellow">
                   <div class="circle-clipper left">
                       <div class="circle"></div>
@@ -68,7 +68,7 @@
                       <div class="circle"></div>
                   </div>
               </div>
-            
+
               <div class="spinner-layer spinner-green">
                   <div class="circle-clipper left">
                       <div class="circle"></div>
@@ -91,25 +91,13 @@
                 <div class="col-sm-12">
                     <!-- Authentication card start -->
                     <form class="md-float-material form-material">
-                        <div class="text-center">
-                            <img src="{{ asset('plugin/cms/assets/images/logo.png') }}" alt="logo.png">
-                        </div>
                         <div class="auth-box card">
                             <div class="card-block">
                                 <div class="row m-b-20">
                                     <div class="col-md-12">
-                                        <h3 class="text-center txt-primary">Sign In</h3>
+                                        <h3 class="text-center txt-primary">LOGIN</h3>
                                     </div>
                                 </div>
-                                <div class="row m-b-20">
-                                    <div class="col-md-6">
-                                        <button class="btn btn-facebook m-b-20 btn-block"><i class="icofont icofont-social-facebook"></i>facebook</button>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <button class="btn btn-twitter m-b-20 btn-block"><i class="icofont icofont-social-twitter"></i>twitter</button>
-                                    </div>
-                                </div>
-                                <p class="text-muted text-center p-b-5">Sign in with your regular account</p>
                                 <div class="form-group form-primary">
                                     <input type="text" name="user-name" class="form-control" required="">
                                     <span class="form-bar"></span>
@@ -136,7 +124,7 @@
                                 </div>
                                 <div class="row m-t-30">
                                     <div class="col-md-12">
-                                        <button type="button" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">LOGIN</button>
+                                        <a href="{{route('cms')}}" type="button" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">LOGIN</a>
                                         <a href="{{ route('cms') }}" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">CMS PAGE</a>
                                         <a href="{{ route('blank') }}" class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">BLANK PAGE</a>
                                     </div>
@@ -155,7 +143,7 @@
         </div>
         <!-- end of container-fluid -->
     </section>
-   
+
 <!-- Required Jquery -->
 <script src="{{ asset('plugin/cms/bower_components/jquery/js/jquery.min.js') }}"></script>
 <script src="{{ asset('plugin/cms/bower_components/jquery-ui/js/jquery-ui.min.js') }}"></script>
