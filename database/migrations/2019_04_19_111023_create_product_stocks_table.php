acca56021a78a4d5f9f702fab082c6d82e7b9bd8
Sok Hai <sokhai.cambodia@gmail.com>
2019-04-19 18:14:21 +0700

    updated: - redesing transaction db schema

--- database/migrations/2019_04_19_111023_create_product_stocks_table.php
+++ database/migrations/2019_04_19_111023_create_product_stocks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInvoiceDetailsTable extends Migration
+class CreateProductStocksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,15 @@ class CreateInvoiceDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('invoice_details', function (Blueprint $table) {
-            
+        Schema::create('product_stocks', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
 
-            $table->bigIncrements('id');
-            $table->integer('invoice_id');
-            $table->integer('product_id');
-            $table->integer('quanity');
-            $table->double('quanity_for_cut_stock', 8, 2);
-            $table->float('price', 8, 2);
-            $table->integer('unit_id');
+            $table->integer('branch_id');
+            $table->integer('product_id'); 
+            $table->double('qty', 8, 2); 
+            $table->primary(['branch_id', 'product_id']);
         });
     }
 
@@ -36,6 +32,6 @@ class CreateInvoiceDetailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('invoice_details');
+        Schema::dropIfExists('product_stocks');
     }
 }
--- database/migrations/2019_04_19_111105_create_invoice_ingredient_details_table.php
+++ database/migrations/2019_04_19_111105_create_invoice_ingredient_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInventoriesTable extends Migration
+class CreateInvoiceIngredientDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,18 @@ class CreateInventoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('inventories', function (Blueprint $table) {
-            
+        Schema::create('invoice_ingredient_details', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
-
+            
             $table->bigIncrements('id');
+            $table->integer('invoice_detail_id');
+            $table->integer('product_id');
+            $table->integer('product_ingredient_id');
+            $table->integer('qty');
+            $table->double('quanity_for_cut_stock', 8, 2);
+            $table->integer('unit_id');
             $table->timestamps();
         });
     }
@@ -31,6 +36,6 @@ class CreateInventoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('inventories');
+        Schema::dropIfExists('invoice_ingredient_details');
     }
 }
